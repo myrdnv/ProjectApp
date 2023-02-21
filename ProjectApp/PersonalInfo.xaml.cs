111def55612dc1f3a9734e89851ae1c7f37acd99
m.yordanov23 <m.yordanov23@LABSCIFIPC16>
2023-02-21 12:43:52 +0200

    Add project files.

---
+++ ProjectApp/PersonalInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectApp
+{
+    /// <summary>
+    /// Interaction logic for PersonalInfo.xaml
+    /// </summary>
+    public partial class PersonalInfo : Window
+    {
+        public PersonalInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
